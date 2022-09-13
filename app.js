ee11818b447fe87df2e1d6ace844ad6d843577d3
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-12 17:35:51 -0700

    gameState: guess

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 
 /* State */
+let gameState = 'guess';
+let guess = '';
+let answer = '';
+let result = '';
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayShells();
+    // displayResults();
+    // displayScoreboard();
+}
 
 /* Components */
 
 /* Component */
 // get DOM
+const guess1 = document.getElementById('guess-1');
+const guess2 = document.getElementById('guess-2');
+const guess3 = document.getElementById('guess-3');
+
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+
+const display1 = document.getElementById('display-1');
+const display2 = document.getElementById('display-2');
+const display3 = document.getElementById('display-3');
+
 // display
+
+function displayShells() {
+    if (gameState === 'guess') {
+        pearl1.classList.add('hidden');
+        pearl2.classList.add('hidden');
+        pearl3.classList.add('hidden');
+        shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
+        display1.classList.add('hidden');
+        display2.classList.add('hidden');
+        display3.classList.add('hidden');
+    }
+}
 // event listeners
 
 /* Run page load code */
