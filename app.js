99fbbc50e4b24a032805aeb4e2214956cee77a97
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-22 15:40:36 -0800

    refactored

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const guess1 = document.getElementById('guess-1');
 const guess2 = document.getElementById('guess-2');
 const guess3 = document.getElementById('guess-3');
 
+const guesses = document.getElementById('guesses');
+
 const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
 const shell3 = document.getElementById('shell-3');
@@ -14,18 +16,16 @@ const pearl2 = document.getElementById('pearl-2');
 const pearl3 = document.getElementById('pearl-3');
 
 const display1 = document.getElementById('display-1');
-const display2 = document.getElementById('display-2');
-const display3 = document.getElementById('display-3');
 
-// const resultsSection = document.getElementById('results');
 const playAgainButton = document.getElementById('play-again-button');
 const winsDisplay = document.getElementById('wins-display');
 const lossesDisplay = document.getElementById('losses-display');
 const totalDisplay = document.getElementById('total-display');
+
 /* State */
 let gameState = 'find'; //'find', 'results'
 let userGuess = ''; // shell-1, shell2, shell3
-let flip = ''; //'shell-1','shell-2','shell-3' //or should it be 'pearl1','pearl2','pearl3'?
+let flip = ''; // 'pearl1','pearl2','pearl3'?
 
 let wins = 0;
 let losses = 0;
@@ -42,6 +42,11 @@ function revealPearl() {
     gameState = 'results';
     flip = getRandomItem(underShell);
     flip.classList.remove('hidden');
+    if (flip === pearl1) {
+        flipOne();
+    } else if (flip === pearl2) {
+        flipTwo();
+    } else flipThree();
     loadPage();
 }
 
@@ -54,124 +59,89 @@ function displayShells() {
         pearl1.classList.add('hidden');
         pearl2.classList.add('hidden');
         pearl3.classList.add('hidden');
+
         shell1.classList.remove('reveal');
         shell2.classList.remove('reveal');
         shell3.classList.remove('reveal');
+
         display1.classList.add('hidden');
-        display2.classList.add('hidden');
-        display3.classList.add('hidden');
+
         playAgainButton.classList.add('hidden');
-        guess1.classList.remove('hidden');
-        guess2.classList.remove('hidden');
-        guess3.classList.remove('hidden');
+        guesses.classList.remove('hidden');
     }
 
     if (gameState === 'results') {
-        guess1.classList.add('hidden');
-        guess2.classList.add('hidden');
-        guess3.classList.add('hidden');
+        guesses.classList.add('hidden');
         playAgainButton.classList.remove('hidden');
     }
 }
+
+function winner() {
+    display1.textContent = 'You did it!';
+    display1.classList.remove('hidden');
+    wins++;
+}
+
+function loser() {
+    display1.textContent = 'Not there!';
+    display1.classList.remove('hidden');
+    losses++;
+}
+
+function flipTwo() {
+    shell2.classList.add('reveal');
+}
+
+function flipOne() {
+    shell1.classList.add('reveal');
+}
+
+function flipThree() {
+    shell3.classList.add('reveal');
+}
+
 function displayResults() {
-    if (userGuess === 'shell1' && flip === pearl1) {
-        shell1.classList.add('reveal');
-        display1.classList.remove('hidden');
-        display1.textContent = 'You did it!';
-        wins++;
-    }
-    if (userGuess === 'shell1' && flip === pearl2) {
-        shell1.classList.add('reveal');
-        shell2.classList.add('reveal');
-        display1.classList.remove('hidden');
-        display1.textContent = 'Not there!';
-        losses++;
-    }
-    if (userGuess === 'shell1' && flip === pearl3) {
-        shell1.classList.add('reveal');
-        shell3.classList.add('reveal');
-        display1.classList.remove('hidden');
-        display1.textContent = 'Not there!';
-        losses++;
-    }
-
-    if ((userGuess === 'shell2') & (flip === pearl1)) {
-        shell2.classList.add('reveal');
-        shell1.classList.add('reveal');
-        display2.classList.remove('hidden');
-        display2.textContent = 'Not there!';
-        losses++;
-    }
-    if ((userGuess === 'shell2') & (flip === pearl2)) {
-        shell2.classList.add('reveal');
-        display2.classList.remove('hidden');
-        display2.textContent = 'You did it!';
-        wins++;
-    }
-    if ((userGuess === 'shell2') & (flip === pearl3)) {
-        shell2.classList.add('reveal');
-        shell3.classList.add('reveal');
-        display2.classList.remove('hidden');
-        display2.textContent = 'Not there!';
-        losses++;
-    }
-
-    if ((userGuess === 'shell3') & (flip === pearl3)) {
-        shell3.classList.add('reveal');
-        display3.classList.remove('hidden');
-        display3.textContent = 'You found it!';
-        wins++;
-    }
-    if ((userGuess === 'shell3') & (flip === pearl1)) {
-        shell3.classList.add('reveal');
-        shell1.classList.add('reveal');
-        display3.classList.remove('hidden');
-        display3.textContent = 'Not there!';
-        losses++;
-    }
-    if ((userGuess === 'shell3') & (flip === pearl2)) {
-        shell3.classList.add('reveal');
-        shell2.classList.add('reveal');
-        display3.classList.remove('hidden');
-        display3.textContent = 'Not there!';
-        losses++;
+    if (userGuess === flip) {
+        winner();
+    } else {
+        loser();
     }
 }
 
-// event listeners
-
-guess1.addEventListener('click', () => {
-    gameState = 'results';
-    userGuess = 'shell1';
+function results() {
     displayShells();
     revealPearl();
     displayResults();
     displayScoreboard();
-});
-
-guess2.addEventListener('click', () => {
-    gameState = 'results';
-    userGuess = 'shell2';
-    displayShells();
-    revealPearl();
-    displayResults();
-    displayScoreboard();
-});
-
-guess3.addEventListener('click', () => {
-    gameState = 'results';
-    userGuess = 'shell3';
-    displayShells();
-    revealPearl();
-    displayResults();
-    displayScoreboard();
-});
+}
 
 function displayScoreboard() {
     winsDisplay.textContent = wins;
     lossesDisplay.textContent = losses;
     totalDisplay.textContent = wins + losses;
 }
+// event listeners
+
+guess1.addEventListener('click', () => {
+    gameState = 'results';
+    userGuess = pearl1;
+    flipOne();
+    results();
+});
+
+guess2.addEventListener('click', () => {
+    gameState = 'results';
+    userGuess = pearl2;
+    flipTwo();
+    results();
+});
+
+guess3.addEventListener('click', () => {
+    gameState = 'results';
+    userGuess = pearl3;
+    flipThree();
+    results();
+});
 
 playAgainButton.addEventListener('click', () => {
     gameState = 'find';
